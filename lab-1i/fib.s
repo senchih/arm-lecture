1d807b387ccfe9caac1a6cf1ad7ab1f1d7c95b22
senchih <a2215689@yahoo.com.tw>
2015-10-03 17:59:50 +0800

    Implement Fibonacci (non recursive)
    add some assembly code in fib.s

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,30 +13,34 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	cmp r0, #0
+	ble .L3
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	@ R3 = 1
+	mov r3, #1
+	@ R4 = 0
+	mov r4, #0
+	@ R5 = R0 (which counts loop)
+	mov r5, r0
+loop:
+	@ R5 = R5 - 1
+	subs r5, r5, #1
+	@ if(R5 <= 0) goto .L3 (which returns R0)
+	beq .L3
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	@ R0 = R3 + R4
+	add r0, r3, r4
+	@ R4 = R3
+	mov r4, r3
+	@ R3 = R0
+	mov r3, r0
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	@ goto loop
+	b loop
 
 	@ END CODE MODIFICATION
 .L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
 	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
